d83b6933cbdac0b6ac8ada2028d03de94d712e99
sihyun10 <hyhy200170@gmail.com>
2025-05-07 15:43:11 +0900

    feat: clienterror() 함수 구현 #5
    tiny 웹 서버가 오류 상황이 발생했을때 클라이언트에게 오류 응답을 보내주는 코드이다.
    잘못된 요청 or 없는 파일 요청했을 때 이 함수가 실행된다.
    
    클라이언트에게 HTTP 에러 응답을 전송하여 errnum(HTTP 상태코드), shortmsg(상태 메시지),
    longmsg(에러 설명 메시지)를 포함한 HTML 본문을 생성한다. 보기 좋게 에러를 보여주는 함수이다.

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -83,3 +83,22 @@ void doit(int fd)
     serve_dynamic(fd, filename, cgiargs);
   }
 }
+
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg)
+{
+  char buf[MAXLINE], body[MAXBUF];
+
+  sprintf(body, "<html><title>Tiny Error</title>");
+  sprintf(body, "%s<body bgcolor=""ffffff"">\r\n", body);
+  sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+  sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
+  sprintf(body, "%s<hr><em>The tiny Web server</em>\r\n", body);
+
+  sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+  Rio_writen(fd, buf, strlen(buf));
+  sprintf(buf, "Content-type: text/html\r\n");
+  Rio_writen(fd, buf, strlen(buf));
+  sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
+  Rio_writen(fd, buf, strlen(buf));
+  Rio_writen(fd, body, strlen(body));
+}
