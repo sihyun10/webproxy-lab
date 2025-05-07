2c7e0ed3c56b76c5310a05e332efc69706fb3c0f
sihyun10 <hyhy200170@gmail.com>
2025-05-07 16:04:15 +0900

    feat: parse_uri() 함수 구현 #5
    tiny 웹 서버에서 클라이언트가 요청한 URI를 해석(파싱)해서
    요청이 정적인 컨텐츠(static)인지 동적인 컨텐츠(dynamic, CGI 프로그램)인지를 구분하고
    그에 맞는 파일 경로(filename)와 CGI 인자(cgiargs)를 설정해준다.
    URI에 cgi-bin이 없다면 정적 컨텐츠이며 URI를 ./로 시작하는 파일 경로로 반환한다.
    URI에 cgi-bin이 포함되어있다면 동적 컨텐츠이며 ? 기준으로 인자를 분리하고 CGI 파일 경로를 추출한다.
    반환값으로는 1이면 정적, 0이면 동적이다.

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -114,3 +114,31 @@ void read_requesthdrs(rio_t *rp)
   }
   return;
 }
+
+int parse_uri(char *uri, char *filename, char *cgiargs)
+{
+  char *ptr;
+
+  if (!strstr(uri, "cgi-bin")) {
+    strcpy(cgiargs, "");
+    strcpy(filename, ".");
+    strcat(filename, uri);
+    if (uri[strlen(uri)-1] == "/") {
+      strcat(filename, "home.html");
+    }
+    return 1;
+  }
+  else {
+    ptr = index(uri, '?');
+    if (ptr) {
+      strcpy(cgiargs, ptr+1);
+      *ptr = '\0';
+    }
+    else {
+      strcpy(cgiargs, "");
+    }
+    strcpy(filename, ".");
+    strcat(filename, uri);
+    return 0;
+  }
+}
